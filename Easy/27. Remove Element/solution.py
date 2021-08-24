63a735f79dc50d04e36840b05c9c774d5b0cd7cd
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2021-08-24 14:57:14 +0530

    Updated

--- Easy/27. Remove Element/solution.py
+++ Easy/27. Remove Element/solution.py
@@ -1,8 +1,8 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        count = 0
-        for i in range(len(nums)):
-            if nums[i]!=val:
-                nums[count] = nums[i]
-                count +=1
-        return count
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.pop(i)
+            else:
+                i += 1
