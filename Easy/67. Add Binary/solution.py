0909efd19dba264e8d9016683abadb4e9b0ae48a
Rubal0990 <rubal00009@gmail.com>
2021-08-29 13:51:23 +0530

    Add Binary

---
+++ Easy/67. Add Binary/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        result = ""
+        carry = 0
+        countOfa = len(a)-1
+        countOfb = len(b)-1
+        while countOfa >= 0 or countOfb >= 0:
+            totalSum = carry
+            if countOfa >= 0:
+                totalSum += int(a[countOfa])
+                countOfa -= 1
+            if countOfb >= 0:
+                totalSum += int(b[countOfb])
+                countOfb -= 1
+            result += str(totalSum%2)
+            carry = totalSum // 2
+            
+        if carry > 0:
+            return str(1) + result
+        return result
