b22eaff0c46dd75d8b25592d90190d1f17e3488e
Rubal0990 <rubal00009@gmail.com>
2021-09-15 12:17:43 +0530

    Valid Palindrome

---
+++ Easy/125. Valid Palindrome/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = ''.join(filter(str.isalnum, s))
+                
+        if s == s[::-1]:
+            return True
+        
+        return False
