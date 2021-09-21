82b9c760eee376f380ca33fa33290f2a57fa2998
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2021-09-21 14:59:18 +0530

    Updated

--- 14 Days of DSA/Day 3/121. Best Time to Buy and Sell Stock/solution.py
+++ 14 Days of DSA/Day 3/121. Best Time to Buy and Sell Stock/solution.py
@@ -1,9 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         maxProfit = 0
-        minsoFar = prices[0]
-        for i in range(0, len(prices)):
-            minsoFar = min(minsoFar, prices[i])
-            profit = prices[i] - minsoFar
+        minsofar = prices[0]
+        
+        for i in range(len(prices)):
+            minsofar = min(minsofar, prices[i])
+            profit = prices[i] - minsofar
             maxProfit = max(maxProfit, profit)
+        
         return maxProfit
