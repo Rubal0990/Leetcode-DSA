afaaad10543ed6c93b61b9ed0069d4759a83783e
Rubal0990 <rubal00009@gmail.com>
2021-09-24 21:15:26 +0530

    First Unique Character in a String

---
+++ 14 Days of DSA/Day 6/387. First Unique Character in a String/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        for i in range(len(s)):
+            check = s[0:i] + s[i+1:]
+            if s[i] in check:
+                continue
+            else:
+                return i
+        return -1
