1ddaa81f5e04beb0c7dacff0437f6805506b5eab
Rubal0990 <rubal00009@gmail.com>
2021-11-06 01:32:13 +0530

    Summary Ranges

---
+++ Easy/228. Summary Ranges/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        result, n = [], len(nums)
+        if n == 0: return result
+        a = nums[0]
+        
+        for i in range(n):
+            if i == n-1 or nums[i]+1 != nums[i+1]:
+                if nums[i] != a: 
+                    result.append(str(a) + "->" + str(nums[i]))
+                else:
+                    result.append(str(a))
+                
+                if i != n-1:
+                    a = nums[i+1]
+                    
+        return result
