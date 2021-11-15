f925e63b64d9c5d9fa3fa8a52fdd5e1884283deb
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2021-11-15 23:43:09 +0530

    Updated

--- Medium/17. Letter Combinations of a Phone Number/solution.py
+++ Medium/17. Letter Combinations of a Phone Number/solution.py
@@ -1,19 +1,23 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        mapping = {"2":"abc", "3":"def", "4":"ghi",
-                    "5":"jkl", "6":"mno", "7":"pqrs",
-                    "8":"tuv", "9":"wxyz"}
+        res = []
+        digitToChar = { "2":"abc",
+                        "3":"def",
+                        "4":"ghi",
+                        "5":"jkl", 
+                        "6":"mno", 
+                        "7":"pqrs",
+                        "8":"tuv", 
+                        "9":"wxyz" }
         
-        def maker( digit_string ):
-            if digit_string == "":
-                return []
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            for c in digitToChar[digits[i]]:
+                backtrack(i+1, curStr+c)
             
-            elif digit_string in mapping:
-                return [*mapping[digit_string]]
-            
-            this_round = mapping[digit_string[0]]
-            next_round = maker(digit_string[1:])
-            
-            return [cur_comb + next_comb for cur_comb in this_round for next_comb in next_round]
+        if digits:
+            backtrack(0, "")
         
-        return maker(digits)
+        return res
