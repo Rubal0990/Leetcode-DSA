e1d5f2e9530843a1301c6ae09a948890e9e0ade5
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-06-14 09:05:53 +0530

    Time: 69 ms (29.95%), Space: 14.2 MB (90.49%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,8 +1,24 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        for i in range(len(nums)):
-            if target == nums[i]:
-                return i
+        return self.helper(nums, 0, len(nums)-1, target)
+    
+    def helper(self, arr: List[int], l: int, h: int, key: int):
+        if l > h:
+            return -1
         
-        return -1
+        mid = (l + h) // 2
+        if arr[mid] == key:
+            return mid
+
+        if arr[l] <= arr[mid]:
+            if key >= arr[l] and key <= arr[mid]:
+                return self.helper(arr, l, mid-1, key)
+            
+            return self.helper(arr, mid + 1, h, key)
+
+        if key >= arr[mid] and key <= arr[h]:
+            return self.helper(arr, mid + 1, h, key)
+
+        return self.helper(arr, l, mid-1, key)
+                    
             
