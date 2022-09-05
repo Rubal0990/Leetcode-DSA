d8607553d32a832ac512263fc0fa848d9a5cf7dc
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-09-05 10:38:46 +0530

    Time: 165 ms (95.71%), Space: 20.6 MB (5.37%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -1,13 +1,17 @@
 class Solution:
     def minDifficulty(self, jd: List[int], d: int) -> int:
+        
         @cache
-        def res(pos = 0, d = d, mx = 0):
-            if d == 0 and pos != len(jd):
+        def res(pos=0, d=d, mx=0):
+            if d==0 and pos!=len(jd):
                 return math.inf
-            elif d == 0 and pos == len(jd):
+            
+            elif d==0 and pos==len(jd):
                 return 0
-            elif d == 0 or pos == len(jd):
+            
+            elif d==0 or pos==len(jd):
                 return math.inf
+            
             else:
                 return min(res(pos+1, d, max(mx, jd[pos])), max(mx, jd[pos]) + res(pos+1, d-1, 0))
         
