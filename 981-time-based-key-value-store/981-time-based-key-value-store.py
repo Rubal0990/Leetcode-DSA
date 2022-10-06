3fbe011d3518521a12d6d0ccdacdf19fb8b1e91b
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-10-06 18:46:23 +0530

    Time: 2060 ms (13.32%), Space: 72 MB (42.81%) - LeetHub

---
+++ 981-time-based-key-value-store/981-time-based-key-value-store.py
@@ -0,0 +1,23 @@
+from sortedcontainers import SortedList
+
+class TimeMap:
+
+    def __init__(self):
+        self.h = defaultdict(lambda: SortedList())
+        self.defaultvalue = ''.join('z'*100)
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.h[key].add((timestamp, value))
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        i = self.h[key].bisect_right((timestamp, self.defaultvalue))
+        return self.h[key][i - 1][1] if i > 0 else ""
+        
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
