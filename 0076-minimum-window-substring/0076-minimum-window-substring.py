1432880557702d4cf899ae324de6b2ee86b4de7e
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-10-22 12:31:40 +0530

    Time: 1268 ms (5.89%), Space: 14.6 MB (98.93%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,34 +1,19 @@
+from collections import Counter
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        lst = [0] * 58
-        tr = [0] * 58
-        st, end, ans = 0, 0, ""
+        tCounter = Counter(t)
+        window = Counter()
+        ans = ""
+        last = 0
         
-        for i in t:
-            lst[ord(i)-ord('A')] += 1
-        
-        while st <= end and end <= len(s):
-            flag = 0
-            for i in range(58):
-                if lst[i] < tr[i]:
-                    flag = 1
-                
-                elif lst[i] > tr[i]:
-                    if end >= len(s):
-                        return ans
-                    
-                    tr[ord(s[end])-ord('A')] += 1
-                    end += 1
-                    break
-            
-            else:
-                if flag == 0:
-                    return s[st:end]
-                
-                if ans == "" or len(ans) > len(s[st:end]):
-                    ans = s[st:end]
-                
-                tr[ord(s[st])-ord('A')] -= 1
-                st += 1
-        
+        for i,char in enumerate(s):
+            window[char] = window.get(char, 0) + 1
+            while window >= tCounter:
+                if ans == "" or i - last < len(ans):
+                    ans = s[last:i+1]
+		    	
+                window[s[last]] -= 1
+                last += 1
+	    
         return ans
