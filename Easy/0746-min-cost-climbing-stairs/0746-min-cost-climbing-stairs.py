5cb314f8a198af0287a3a82280515c06451e1ecd
Rubal Bansal <rubal00009@gmail.com>
2022-11-20 08:24:29 +0530

    Python - Easy

---
+++ Easy/0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        a = b = 0
+        for c in cost:
+            a,b = b, min(a, b) + c
+        
+        return min(a, b)
