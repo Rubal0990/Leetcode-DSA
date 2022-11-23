4edf0792f811a5e73213f3efd8d1bb76ae8ad04a
Rubal Bansal <rubal00009@gmail.com>
2022-11-23 19:47:30 +0530

    Python - Easy

---
+++ LeetCode 75 - Level 1/Day 14/0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -0,0 +1,18 @@
+class Solution:
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        l1 = self.stack(S, [])
+        l2 = self.stack(T, [])
+        return l1 == l2
+        
+    
+    def stack(self, S, stack):
+        for char in S:
+            if char is not "#":
+                stack.append(char)
+            else:
+                if not stack:
+                    continue
+                
+                stack.pop()
+        
+        return stack
