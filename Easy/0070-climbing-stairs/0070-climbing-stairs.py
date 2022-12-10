833baa4c5d1912967250d15fdc2b3233be54bfa8
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-12-10 17:56:11 +0530

    Update 0070-climbing-stairs.py

--- Easy/0070-climbing-stairs/0070-climbing-stairs.py
+++ Easy/0070-climbing-stairs/0070-climbing-stairs.py
@@ -2,7 +2,7 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         path = {1:1, 2:2}
         
-        for i in range(4, n+1):
+        for i in range(3, n+1):
             path[i] = path[i-1] + path[i-2]
         
         return path[n]
