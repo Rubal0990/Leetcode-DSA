3dcafd8c72090eaf0d8ef39b37bcea65242212d4
Rubal Bansal <rubal00009@gmail.com>
2022-12-23 20:02:37 +0530

    Python - Medium

---
+++ Medium/0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) < 2: 
+            return 0
+        
+        prev_sell, sell, buy = 0, 0, -prices[0]
+        for i in range(1, len(prices)): 
+            prev_sell, sell, buy = sell, max(buy+prices[i], sell), max(prev_sell-prices[i], buy) 
+        
+        return max(sell, buy)
