818017b8a5ed3573e666fecee13073415681bc59
Rubal Bansal <rubal00009@gmail.com>
2022-12-26 08:26:42 +0530

    Python - Medium

---
+++ Medium/0055-jump-game/0055-jump-game.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if len(nums) <= 1:
+            return True
+        
+        j = len(nums) - 2
+        i = len(nums) - 1    
+        while j > -1:
+            if j + nums[j] >= i:
+                i = j
+                j -= 1
+                
+            else:
+                j -= 1
+        
+        if i <= 0:
+            return True
+        else:
+            return False
