6b487c3533b3e22e66cd791dcb1e659e0d60724a
Rubal Bansal <rubal00009@gmail.com>
2023-01-06 11:28:03 +0530

    Python - Medium

---
+++ Medium/1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+        res = 0
+        
+        for price in costs:
+            if price <= coins:
+                res += 1
+                coins -= price
+            
+            else:
+                break
+        
+        return res
