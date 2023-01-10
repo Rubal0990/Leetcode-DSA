d3288c96745de9b16e3a478c7d245ebf551741bc
Rubal Bansal <rubal00009@gmail.com>
2023-01-10 10:19:00 +0530

    Python - Easy

--- Easy/0100-same-tree/0100-same-tree.py
+++ Easy/0100-same-tree/0100-same-tree.py
@@ -8,27 +8,29 @@ class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if p is None and q is None:
             return True
+        
         if p is None:
             return False
+        
         if q is None:
             return False
+        
         stack = deque()
-        stack.append((p,q))
+        stack.append((p, q))
+        
         while stack:
-            p,q = stack.pop()
+            p, q = stack.pop()
             
             if p.val != q.val:
                 return False
             
             if p.left and q.left:
-                stack.append((p.left, q.left))
-                
+                stack.append((p.left, q.left))  
             elif p.left or q.left:
                 return False
             
             if p.right and q.right:
                 stack.append((p.right, q.right))
-                
             elif p.right or q.right:
                 return False
             
