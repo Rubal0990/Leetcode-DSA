836991109e0fdf2e232602b5ef0374872fa750af
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2023-01-11 10:12:56 +0530

    Update 1443-minimum-time-to-collect-all-apples-in-a-tree.py

--- Medium/1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
+++ Medium/1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
@@ -1,24 +1,19 @@
 class Solution:
-    def myAtoi(self, s: str) -> int:
-        pattern = re.compile(r'[-+]?[0-9]+')
-        s = s.strip()
-        word_list = pattern.findall(s)
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        tree = defaultdict(list)
+        for s, e in edges:
+            tree[s].append(e)
+            tree[e].append(s)
         
-        if word_list:
-            value = word_list[0]
-            if s.startswith(value):
-                value = int(value)
-                if value > 2**(31)-1:
-                    return 2**(31)-1
-                
-                elif value < -2**(31):
-                    return -2**(31)
-                
-                else:
-                    return value
+        def dfs(node, par):
+            res = 0
+            for nei in tree[node]:
+                if nei != par:
+                    res += dfs(nei, node)
             
-            else:
-                return 0
+            if res or hasApple[node]:
+                return res + 2
+            
+            return res
         
-        else:
-            return 0
+        return max(dfs(0, -1) - 2, 0)
