a1d153578765bad55e87faa88faca1d14f3fa9c0
Rubal Bansal <rubal00009@gmail.com>
2023-03-03 23:17:27 +0530

    Python - Medium

---
+++ Medium/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if haystack == None or needle == None:
+            return 0
+        
+        if haystack == needle:
+            return 0
+        
+        for i in range(len(haystack)):
+            if needle == haystack[i:i + len(needle)]:
+                return i
+        
+        return -1
