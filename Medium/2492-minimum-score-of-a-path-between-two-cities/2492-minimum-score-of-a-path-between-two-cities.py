8f44149e98100ffcf983db5681a952356bb772a9
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2023-03-22 12:08:40 +0530

    Update 2492-minimum-score-of-a-path-between-two-cities.py

--- Medium/2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ Medium/2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,9 +1,21 @@
 class Solution:
-    def convert(self, s: str, numRows: int) -> str:
-        template = list(range(numRows)) + list(range(numRows - 2, 0, -1))
-        result = [''] * numRows
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        graph = defaultdict(dict)
+        for u, v, w in roads:
+            graph[u][v] = graph[v][u] = w
         
-        for i, char in enumerate(s):
-            result[template[i % len(template)]] += char
+        min_score = float('inf')
+        visited = set()
+        queue = deque([1])
         
-        return ''.join(result)
+        while queue:
+            node = queue.popleft()
+            
+            for adj, score in graph[node].items():
+                if adj not in visited:
+                    queue.append(adj)
+                    visited.add(adj)
+                
+                min_score = min(min_score, score)
+                
+        return min_score
