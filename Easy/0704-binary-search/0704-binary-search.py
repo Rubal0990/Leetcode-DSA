5ed81db01d07fa063e332d34aaa4b9a823b5d390
Rubal Bansal <rubal00009@gmail.com>
2023-04-01 16:55:30 +0530

    Python - Easy

--- Easy/0704-binary-search/0704-binary-search.py
+++ Easy/0704-binary-search/0704-binary-search.py
@@ -1,15 +1,18 @@
+from typing import List
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        lo, hi = 0, len(nums) - 1
-        
-        while lo <= hi:
-            mid = (lo + hi) // 2
-            
-            if nums[mid] > target:
-                hi = mid - 1
-            elif nums[mid] < target:
-                lo = mid + 1
+        l = 0
+        r = len(nums) - 1
+
+        while l < r:
+            mid = l + (r - l) // 2
+            if nums[mid] >= target:
+                r = mid
             else:
-                return mid
+                l = mid + 1
+
+        if nums[l] != target:
+            return -1
         
-        return -1
+        return l
