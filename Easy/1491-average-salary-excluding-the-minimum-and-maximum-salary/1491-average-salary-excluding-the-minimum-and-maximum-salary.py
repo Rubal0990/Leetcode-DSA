8f687fe0866a1d7a7db3b6c6f35278a8fbdabe29
Rubal Bansal <rubal00009@gmail.com>
2023-05-01 12:38:09 +0530

    Python - Easy

---
+++ Easy/1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -0,0 +1,8 @@
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        min_salary = min(salary)
+        max_salary = max(salary)
+        total_salary = sum(salary) - min_salary - max_salary
+        num_employees = len(salary) - 2
+        
+        return total_salary / num_employees
