0d096c614f199be3b4731134515fb5a7d1331d2f
Rubal Bansal <rubal00009@gmail.com>
2023-06-12 07:11:24 +0530

    Python - Easy

--- Easy/0228-summary-ranges/0228-summary-ranges.py
+++ Easy/0228-summary-ranges/0228-summary-ranges.py
@@ -1,9 +1,10 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         result, n = [], len(nums)
-        if n == 0: return result
-        a = nums[0]
+        if n == 0: 
+            return result
         
+        a = nums[0]
         for i in range(n):
             if i == n-1 or nums[i]+1 != nums[i+1]:
                 if nums[i] != a: 
